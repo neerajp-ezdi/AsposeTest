52a0f267c0acb2ca2541e250244dad41da5d6deb
neerajp-ezdi <neeraj.p@ezdi.us>
2018-02-12 18:59:41 +0530

    Added code for insertAfter

--- src/main/java/com/ezdi/aspose/controller/ExtractContentAndReplaceWithBookmark.java
+++ src/main/java/com/ezdi/aspose/controller/ExtractContentAndReplaceWithBookmark.java
@@ -17,7 +17,7 @@ public class ExtractContentAndReplaceWithBookmark {
         try{
             System.out.println("Now inside extractContentBetweenServices");
             String dataDir = Util.getDataDir();
-            Document doc = new Document(dataDir + "Template.docx");
+            Document doc = new Document(dataDir + "bmaninder.docx");
             //NodeCollection<Paragraph> paragraphCollection = doc.getFirstSection().getBody().getChildNodes(NodeType.PARAGRAPH, true);
             NodeCollection nodeCollection = doc.getFirstSection().getBody().getChildNodes();
             for(Node node : (Iterable<Node>) nodeCollection){
@@ -27,6 +27,38 @@ public class ExtractContentAndReplaceWithBookmark {
                             && ProcessData.isValid(paragraph.getRuns().get(0).getText())){
                         System.out.println("Paragraph text is now " + paragraph.getRuns().get(0).getText()
                                 + " and paragraph index is now " + paragraph.getAncestor(NodeType.BODY).indexOf(paragraph));
+                        if(paragraph.getRuns().get(0).getText().contains("==START==")){
+                            Paragraph newParagraph = new Paragraph(doc);
+                            //doc.getFirstSection().getBody().appendChild(newParagraph);
+                            Node nodeParent  = node.getParentNode();
+                            System.out.println("Node parent is now " + nodeParent.getNodeType() + " and text is " + nodeParent.toString());
+                            Run run = new Run(doc);
+                            run.setText("BODY_CONTENT");
+                            newParagraph.appendChild(run);
+                            doc.getFirstSection().getBody().insertAfter(newParagraph, node);
+                        }
+                    }
+                } else {
+                    System.out.println("Node is not of type paragraph. The NodeType is " + node.getNodeType() + " and text is " + node.toString() );
+                }
+            }
+
+            System.out.println("Now printing after iterating the second time ");
+            NodeCollection newNodeCollection = doc.getFirstSection().getBody().getChildNodes();
+            for(Node node : (Iterable<Node>) newNodeCollection){
+                if(node.getNodeType() == NodeType.PARAGRAPH) {
+                    Paragraph paragraph = (Paragraph) node;
+                    if(ProcessData.isValid(paragraph.getRuns()) && ProcessData.isValid(paragraph.getRuns().get(0))
+                            && ProcessData.isValid(paragraph.getRuns().get(0).getText())){
+                        System.out.println("Paragraph text is now " + paragraph.getRuns().get(0).getText()
+                                + " and paragraph index is now " + paragraph.getAncestor(NodeType.BODY).indexOf(paragraph));
+                        /*if(paragraph.getRuns().get(0).getText().contains("==START==")){
+                            Paragraph newParagraph = new Paragraph(doc);
+                            Run run = new Run(doc);
+                            run.setText("BODY_CONTENT");
+                            newParagraph.appendChild(run);
+                            doc.insertAfter(newParagraph, node);
+                        } */
                     }
                 } else {
                     System.out.println("Node is not of type paragraph. The NodeType is " + node.getNodeType() + " and text is " + node.toString() );
